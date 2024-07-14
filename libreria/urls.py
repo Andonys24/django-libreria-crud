8385e7db606b0168bb0724d023c6b5982febfdbe
Andonys24 <andonihernandez1990@gmail.com>
2024-07-13 19:58:16 -0600

    Plantilla template para la pagina web Mostrando una tabla con HTML Creando un menu de navegacion para todas las paginas Acceso a la vista de la pagina web Incluyendo el formulario en crear y editr libro creacion de paginas de: libros: crear, editar, index y un formulario

--- libreria/urls.py
+++ libreria/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.incio, name='inicio'),
-    path('nosotros', views.nosotros, name='nosotros')
+    path("", views.incio, name="inicio"),
+    path("nosotros", views.nosotros, name="nosotros"),
+    path("libros", views.libros, name="libros"),
+    path("libros/crear", views.crear_libro, name="crear-libro"),
+    path("libros/editar", views.editar_libro, name="editar-libro"),
 ]
--- libreria/views.py
+++ libreria/views.py
@@ -4,8 +4,20 @@ from django.http import HttpResponse
 
 # Create your views here.
 def incio(request):
-    return HttpResponse("<h1>Bienvenido Develoteca</h1>")
+    return render(request, "paginas/inicio.html")
 
 
 def nosotros(request):
     return render(request, "paginas/nosotros.html")
+
+
+def libros(request):
+    return render(request, "libros/index.html")
+
+
+def crear_libro(request):
+    return render(request, "libros/crear.html")
+
+
+def editar_libro(request):
+    return render(request, "libros/editar.html")
