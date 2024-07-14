21a324fbb485859e64e213c30c695b0da1dbecb8
Andonys24 <andonihernandez1990@gmail.com>
2024-07-13 21:42:24 -0600

    Generando Formulario de manera Dinamica - Generando el formulario con las columans de la     Base de datos de manera dinamica - Trayendo a la vista el formularo con los typos pertenecientes

--- libreria/views.py
+++ libreria/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Libro
+from .forms import LibroForm
 
 
 # Create your views here.
@@ -18,7 +19,8 @@ def libros(request):
 
 
 def crear_libro(request):
-    return render(request, "libros/crear.html")
+    formulario = LibroForm(request.POST or None)
+    return render(request, "libros/crear.html", {"formulario": formulario})
 
 
 def editar_libro(request):
